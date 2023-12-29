b85feede479e3ff96b01337abc72585afc5e9313
Mohlahrech <124674380+Mohlahrech@users.noreply.github.com>
2023-12-29 19:18:37 +0100

    Delete voltere_projet directory

--- voltere_projet/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class resPartnerDob(models.Model):
-    _inherit = 'res.partner'
-    _description = 'nouveaux champs'
-
-    dob = fields.Date(string='Date de Naissance', required=True)
-    lieu = fields.Char(string='Lieu de naissance', required=True)
